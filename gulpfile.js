caa1c08048263ead985445c87dfbaf82f32216db
Sujay Khandekar <khandek2@illinois.edu>
2016-08-29 17:55:00 -0500

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
 var server = require('gulp-webserver');
-var clean = require('gulp-clean');
 
 gulp.task('sass', function () {
-    gulp.src('source_sass/*.scss')
+    gulp.src('sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('public/css/'));
-});
-
-gulp.task('clean-scripts', function () {
-    return gulp.src('public/js/*.js', {read: false})
-               .pipe(clean());
-});
-
-gulp.task('js', ['clean-scripts'], function() {
-  gulp.src('source_js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('public/js/'));
+        .pipe(gulp.dest('css/'));
 });
 
 gulp.task('serve', function () {
@@ -31,7 +18,8 @@ gulp.task('serve', function () {
 
 gulp.task('watch', function () {
     gulp.watch('source_sass/*.scss', ['sass']);
-    gulp.watch('source_js/*.js', ['js']);
 });
 
-gulp.task('default', ['sass', 'js', 'serve', 'watch']);
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['sass', 'serve', 'watch']);
